0045451217401d07b940ef6fabb1309c8553504c
XiangrongXu <sherlockhsuxxr@gmail.com>
2021-04-30 16:45:08 +0800

    21_04_30_PC

--- softmax_net.py
+++ softmax_net.py
@@ -21,6 +21,18 @@ def init_weights(m):
     if type(m) == nn.Linear:
         nn.init.norml_(m.weight, std=0.01)
 
+
+def train_epoch_ch3(net, train_iter, loss, trainer):
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        trainer.zero_grap()
+        l.backward()
+        trainer.step()
+        metric.add(float(l) * len(y), accuracy(y_hat, y), y.numel())
+    return metric[0] / metric[2], metric[1] / metric[2]
+
 batch_size = 256
 mnist_train, mnist_test = load_data_fashoin_mnist(batch_size)
 net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
--- softmax_reg.py
+++ softmax_reg.py
@@ -1,48 +1,86 @@
+from mnist_set import load_data_fashion_mnist
 import torch
-import torchvision
-from torch.utils import data
-from torchvision import transforms
-from d2l import torch as d2l
+from IPython import display
+
+class Accumulator:
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    
+    def __getitem__(self, idx):
+        return self.data[idx]
 
 
-def get_fashion_mnist_labels(labels):
-    text_labels = ["t-shirt", "trouser", "pullover", "dress", "coat", "sandal", "shirt", "sneaker", "bag", "ankle boot"]
-    return [text_labels[int(i)] for i in labels]
+def softmax(X):
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdim=True)
+    return X_exp / partition
 
 
-def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
-    figsize = (num_cols * scale, num_rows * scale)
-    _, axes = d2l.plt.subplots(num_rows, num_cols, figsize=figsize)
-    axes = axes.flatten()
-    for i, (ax, img) in enumerate(zip(axes, imgs)):
-        if torch.is_tensor(img):
-            ax.imshow(img.numpy())
+def net(X):
+    return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
+
+
+def cross_entropy(y_hat, y):
+    return -torch.log(y_hat[range(len(y_hat)), y])
+
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()
+    metric = Accumulator(2)
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.backward()
+            updater.step()
+            metric.add(float(l) * len(y), accuracy(y_hat, h), y.size().numel)
         else:
-            ax.imshow(img)
-        ax.axes.get_xaxis().set_visible(False)
-        ax.axes.get_yaxis().set_visible(False)
-        if titles:
-            ax.set_title(titles[i])
-    return axes
+            l.sum().backward()
+            updater(X.shape[0])
+            metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    return metric[0] / metric[2], metric[1] / metric[2]
 
 
-def get_dataloader_workers():
-    return 4
 
 
-def load_data_fashion_mnist(batch_size, resize=None):
-    trans = [transforms.ToTensor()]
-    if resize:
-        trans.insert(0, transforms.Resize(resize))
-    trans = transforms.Compose(trans)
-    mnist_train = torchvision.datasets.FashionMNIST(root="./data", train=True, transform=trans, download=True)
-    mnist_test = torchvision.datasets.FashionMNIST(root="./data", train=False, transform=trans, download=True)
-    return (data.DataLoader(mnist_train, batch_size, shuffle=True),
-            data.DataLoader(mnist_test, batch_size, shuffle=False))
 
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+num_inputs = 28 * 28
+num_outputs = 10
 
-train_iter, test_iter = load_data_fashion_mnist(32, resize=64)
-for X, y in train_iter:
-    print(X.shape, X.dtype, y.shape, y.dtype)
-    break
+W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+
+X = torch.tensor([[1., 2., 3.], [4., 5., 6.]])
+X_prob = softmax(X)
+print(X_prob, X_prob.sum(1))
 
