f1d779c64f5dfb37efe8f03f901a96115cefb2dc
XiangrongXu <sherlockhsuxxr@gmail.com>
2021-05-08 17:32:53 +0800

    21_05_08PC

--- rnn/text_preprocessing.py
+++ rnn/text_preprocessing.py
@@ -2,6 +2,20 @@ import collections
 import re
 from d2l import torch as d2l
 
-# d2l.DATA_HUB["time_machine"] = (d2l.DATA_URL + "timemachine.txt", "090b5e7e70c295757f55df93cb0a180b9691891a")
-corpus, vocab = d2l.load_corpus_time_machine()
-print(list(vocab.token_to_idx.items()))
+tokens = d2l.tokenize(d2l.read_time_machine())
+corpus = [token for line in tokens for token in line]
+vocab = d2l.Vocab(corpus)
+freqs = [freq for _, freq in vocab.token_freqs]
+
+bigram_tokens = [pair for pair in zip(corpus[:-1], corpus[1:])]
+bigram_vocab = d2l.Vocab(bigram_tokens)
+print(bigram_vocab.token_freqs[:10])
+bifreqs = [freq for _, freq in bigram_vocab.token_freqs]
+
+trigram_tokens = [tup for tup in zip(corpus[:-2], corpus[1:-1], corpus[2:])]
+trigram_vocab = d2l.Vocab(trigram_tokens)
+print(trigram_vocab.token_freqs[:10])
+trifreqs = [freq for _, freq in trigram_vocab.token_freqs]
+
+d2l.plot([freqs, bifreqs, trifreqs], xlabel="token: x", ylabel="frequency: n(x)", xscale="log", yscale="log", legend=["unigram", "bigram", "trigram"])
+d2l.plt.show()
