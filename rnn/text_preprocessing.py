b43d81f8750e787a43ecf6d8b2ddcbc5d50287c0
XiangrongXu <sherlockhsuxxr@gmail.com>
2021-05-12 17:53:59 +0800

    21_05_12PC

--- rnn/text_preprocessing.py
+++ rnn/text_preprocessing.py
@@ -1,6 +1,8 @@
 import collections
 import re
 from d2l import torch as d2l
+import random
+import torch
 
 tokens = d2l.tokenize(d2l.read_time_machine())
 corpus = [token for line in tokens for token in line]
@@ -19,3 +21,32 @@ trifreqs = [freq for _, freq in trigram_vocab.token_freqs]
 
 d2l.plot([freqs, bifreqs, trifreqs], xlabel="token: x", ylabel="frequency: n(x)", xscale="log", yscale="log", legend=["unigram", "bigram", "trigram"])
 d2l.plt.show()
+
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    num_subseqs = (len(corpus) - 1) // num_steps
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    random.shuffle(initial_indices)
+    
+    def data(pos):
+        return corpus[pos:pos + num_steps]
+    
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        initial_indices_per_batch = initial_indices[i:i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+
+def seq_data_iter_sequential(corpus, batch_size, num_steps):
+    offset = random.randint(0, num_steps - 1)
+    num_tokens = (len(corpus) - offset - 1) // batch_size * batch_size
+    Xs = torch.tensor(corpus[offset:offset + num_tokens])
+    Ys = torch.tensor(corpus[offset + 1:offset + 1 + num_tokens])
+    Xs, Ys = Xs.reshape(batch_size, -1), Ys.reshape(batch_size, -1)
+    num_batches = Xs.shape[1] // num_steps
+    for i in range(0, num_steps * num_batches, num_steps):
+        X = Xs[:, i:i + num_steps]
+        Y = Ys[:, i:i + num_steps]
+        yield X, Y
