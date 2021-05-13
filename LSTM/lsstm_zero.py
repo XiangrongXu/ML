5b1aedaf4ea360b933b34408a2101f68ea6c8c7d
XiangrongXu <sherlockhsuxxr@gmail.com>
2021-05-13 18:04:01 +0800

    21_05_13PC

---
+++ LSTM/lsstm_zero.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+batch_size, num_steps = 32, 35
+train_iter, vocab = d2l.load_data_time_machine(batch_size, num_steps)
+
+def get_lstm_params(vocab_size, num_hiddens, device):
+    num_inputs = num_outputs = vocab_size
+
+    def normal(shape):
+        return torch.randn(size=shape, device=device) * 0.01
+    
+    def three():
+        return (normal((num_inputs, num_hiddens)), normal((num_hiddens, num_hiddens)), torch.zeros(num_hiddens, device=device))
+    
+    
